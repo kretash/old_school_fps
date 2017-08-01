c86c1ba7896cadb8ad5a4bb770f49162321ca632
Carlos Martinez Romero <kretash@gmail.com>
2017-08-01 13:52:22 +0100

    smol changes

--- src/main.cc
+++ src/main.cc
@@ -16,7 +16,7 @@ int main( int argc, char *argv[] ) {
     bool running = true;
     const int32_t width = 256;
     const int32_t height = 256;
-    const int32_t scale = 1;
+    const int32_t scale = 3;
     const float ratio_x = ( float ) height / ( float ) width;
 
     auto input = Input::get_instace();
@@ -28,45 +28,9 @@ int main( int argc, char *argv[] ) {
     window->create( width, height, scale );
 
     Texture checker;
-    checker.load("../../assets/checker.png");
+    checker.load( "../../assets/checker.png" );
 
-    Triangle t0, t1;
-
-    {
-        const float s = 1.0f;
-        const float s_x = -s * ratio_x;
-        const float l_x =  s * ratio_x;
-        const float s_y = -s;
-        const float l_y =  s;
-
-        Vertice v0;
-        v0.pos = float3( s_x, s_y, 0.0f );
-        v0.normal = float3( 1.0f, 1.0f, 1.0f );
-        v0.uv = float2( 0.0f, 0.0f );
-
-        Vertice v1;
-        v1.pos = float3( s_x, l_y, 0.0f );
-        v1.normal = float3( 0.0f, 0.0f, 0.0f );
-        v1.uv = float2( 0.0f, 1.0f );
-
-        Vertice v2;
-        v2.pos = float3( l_x, s_y, 0.0f );
-        v2.normal = float3(0.0f, 0.0f, 0.0f);
-        v2.uv = float2( 1.0f, 0.0f );
-
-        Vertice v3;
-        v3.pos = float3( l_x, l_y, 0.0f );
-        v3.normal = float3(1.0f, 0.0f, 0.0f);
-        v3.uv = float2( 1.0f, 1.0f );
-
-        t0.triangle[0] = v0;
-        t0.triangle[1] = v3;
-        t0.triangle[2] = v1;
-
-        t1.triangle[0] = v0;
-        t1.triangle[1] = v3;
-        t1.triangle[2] = v2;
-    }
+    float time = 0.0f;
 
     while( running ) {
         input->update();
@@ -74,12 +38,51 @@ int main( int argc, char *argv[] ) {
 
         renderer->clear( 0xff111111 );
 
-        renderer->bind(&checker);
+        Triangle t0, t1;
+
+        {
+            const float s = 1.0f;
+            const float s_x = -s * ratio_x;
+            const float l_x = s * ratio_x;
+            const float s_y = -s;
+            const float l_y = s;
+
+            Vertice v0;
+            v0.pos = float3( s_x, s_y, 0.0f );
+            v0.normal = float3( 1.0f, 1.0f, 1.0f );
+            v0.uv = float2( 0.0f, 0.0f );
+
+            Vertice v1;
+            v1.pos = float3( s_x, l_y, 0.0f );
+            v1.normal = float3( 0.0f, 0.0f, 0.0f );
+            v1.uv = float2( 0.0f, 1.0f );
+
+            Vertice v2;
+            v2.pos = float3( l_x, s_y, 0.0f );
+            v2.normal = float3( 0.0f, 0.0f, 0.0f );
+            v2.uv = float2( 1.0f, 0.0f );
+
+            Vertice v3;
+            v3.pos = float3( l_x, l_y, 0.0f );
+            v3.normal = float3( 1.0f, 0.0f, 0.0f );
+            v3.uv = float2( 1.0f, 1.0f );
+
+            t0.triangle[0] = v0;
+            t0.triangle[1] = v3;
+            t0.triangle[2] = v1;
+
+            t1.triangle[0] = v0;
+            t1.triangle[1] = v3;
+            t1.triangle[2] = v2;
+        }
+
+        renderer->bind( &checker );
         renderer->render_triangle( t0 );
         renderer->render_triangle( t1 );
 
         auto final_buffer = renderer->get_color_buffer();
         window->present( final_buffer );
+        time += 0.016f;
     }
 
     return 0;
--- src/texture.cc
+++ src/texture.cc
@@ -3,19 +3,22 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "external/stb_image.h"
 
-void Texture::load(const char* file) {
+void Texture::load( const char* file ) {
     n = 4;
-    m_image_data = (uint32_t*) stbi_load(file, &x, &y, &n, 4);
+    m_image_data = ( uint32_t* ) stbi_load( file, &x, &y, &n, 4 );
 }
 
 
-uint32_t Texture::sample(float2 uv) {
+uint32_t Texture::sample( float2 uv ) {
 
-    int32_t sx = (int32_t)(uv.x * (float)x);
-    int32_t sy = (int32_t)(uv.y * (float)y);
+    float scale_x = ( ( float ) x - 1.0f ) / ( float ) x;
+    float scale_y = ( ( float ) y - 1.0f ) / ( float ) y;
 
-    sx = clamp(sx, 0, x);
-    sy = clamp(sy, 0, y);
+    int32_t sx = ( int32_t ) ( uv.x * ( float ) x - 0.5f );
+    int32_t sy = ( int32_t ) ( uv.y * ( float ) y - 0.5f );
+
+    sx = clamp( sx, 0, x - 1 );
+    sy = clamp( sy, 0, y - 1 );
 
     uint32_t color = m_image_data[sy*x + sx];
 
@@ -25,6 +28,6 @@ uint32_t Texture::sample(float2 uv) {
 }
 
 Texture::~Texture() {
-    if (m_image_data != nullptr)
-        stbi_image_free(m_image_data);
+    if( m_image_data != nullptr )
+        stbi_image_free( m_image_data );
 }
